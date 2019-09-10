6d0156f264b7e57cb238bed76a4c76063083f279
Ahana Ghosh <ahanaghosh94@gmail.com>
2019-09-10 16:40:11 +0000

    add feature to account for character counter going below 0 to turn red

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,27 +1,36 @@
 $(document).ready(function () {
   // --- our code goes here ---
 
-  const maximumLength = 140;
+  const maximumLength = parseInt($(".counter").text());
+
+
   $(".tweet-text-area").keyup(function (e) {
     let currentTextLength = $(this).val().length;
+    let remainingLength = maximumLength - currentTextLength;
+    $(".counter").text(remainingLength);
 
-    if (currentTextLength <= maximumLength) {
-      let remainingLength = maximumLength - currentTextLength;
-      $(".counter").text(remainingLength);
+    if (remainingLength < 0) {
+      $(".counter").css('color', 'red');
+
+    } else {
+      $(".counter").css('color', 'black');
+
     }
 
   });
 
-  $(".tweet-text-area").keydown(function (e) {
-    let keycode = e.keyCode;
-    let textLength = $(this).val().length;
-    if (textLength > maximumLength) {
-      if (keycode !== 8) {
-        e.preventDefault();
-        return false;
-      }
-    }
+  // $(".tweet-text-area").keydown(function (e) {
+  //   let keycode = e.keyCode;
+  //   let textLength = $(this).val().length;
+  //   if (textLength > maximumLength) {
+  //     // $(".counter").css('colour', 'red');
+  //     // $(".counter").text(maximumLength - textLength);
+  //     if (keycode !== 8) {
+  //       e.preventDefault();
+  //       return false;
+  //     }
+  //   }
 
 
-  });
+  // });
 });

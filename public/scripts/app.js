c3999f198ea91d94edd44036335a1773f2ebd264
Ahana Ghosh <ahanaghosh94@gmail.com>
2019-09-12 02:10:09 +0000

    add moment.js to better handle days/hours/seconds past since tweet posted

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,7 +3,7 @@
 //  * jQuery is already loaded
 //  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
 //  */
-// const moment = require('./moment');
+
 
 const escape = function (str) {
   let div = document.createElement('div');
@@ -11,15 +11,17 @@ const escape = function (str) {
   return div.innerHTML;
 };
 
-const daysAgo = (dateOfTweet) => {
+const daysAgo = (tweetDate) => {
 
-  let todaysDate = new Date();
-  let days = Math.floor(Math.abs((todaysDate.getTime() - dateOfTweet) / (1000 * 60 * 60 * 24)));
-  if (days < 7 && days > 1) {
-    return (`${days} days ago`);
-  } else {
-    return (new Date(dateOfTweet));
-  }
+  // console.log(moment(tweetDate).fromNow());
+  // let todaysDate = new Date();
+  // let days = Math.floor(Math.abs((todaysDate.getTime() - tweetDate) / (1000 * 60 * 60 * 24)));
+  // if (days < 7 && days > 1) {
+  //   return (`${days} days ago`);
+  // } else {
+  //   return (new Date(tweetDate));
+  // }
+  return moment(tweetDate).fromNow();
 };
 
 const renderTweets = (tweets) => {
@@ -30,24 +32,34 @@ const renderTweets = (tweets) => {
 };
 
 const createTweetElement = (tweet) => {
-  let $tweet = `<article class="tweets">
-  <header>
-  <div>
-  <img src="${escape(tweet.user.avatars)}" class="avatar">
-  <p>${escape(tweet.user.name)}</p>
-  </div>
-  <p class = "Username">${escape(tweet.user.handle)}</p>
-  </header>
-  <p>${escape(tweet.content.text)}</p>
-  <footer>
-    <span class="time">${escape(daysAgo(tweet.created_at))}</span>
-    <span class="icons"><i class="fa fa-flag" width="100px" height="100px"></i>
-      <i class="fa fa-heart"></i>
-      <i class="fa fa-retweet"></i></span>
-  </footer>
-</article>`;
+  let $tweet = (
+    `<article class="tweets">
+      <header>
+      <div>
+      <img src="${escape(tweet.user.avatars)}" class="avatar">
+      <p>${escape(tweet.user.name)}</p>
+      </div>
+      <p class = "Username">${escape(tweet.user.handle)}</p>
+      </header>
+      <p>${escape(tweet.content.text)}</p>
+      <footer>
+        <span class="time">${escape(daysAgo(tweet.created_at))}</span>
+        <span class="icons"><i class="fa fa-flag" width="100px" height="100px"></i>
+          <i class="fa fa-heart"></i>
+          <i class="fa fa-retweet"></i></span>
+      </footer>
+    </article>`
+  );
 
   return $tweet;
+};
+
+const loadTweets = async () => {
+  const response = await $.ajax({
+    url: '/tweets/',
+    type: 'GET',
+  });
+  renderTweets(response);
 };
 
 const validateUserInput = (input) => {
@@ -62,7 +74,6 @@ const validateUserInput = (input) => {
   } else {
     return true;
   }
-
 };
 
 const getUserTweetInput = () => {
@@ -70,10 +81,9 @@ const getUserTweetInput = () => {
   return input.serialize();
 };
 
-
 $('#compose-tweet').submit(async (event) => {
   event.preventDefault();
-  const input = $('textarea').val();
+  const input = $('.tweet-text-area').val();
 
   if (validateUserInput(input)) {
     if ($('p').hasClass('error')) {
@@ -84,22 +94,12 @@ $('#compose-tweet').submit(async (event) => {
     $('.tweet-text-area').keyup();
     loadTweets();
   }
-
 });
-
-
-
-const loadTweets = async () => {
-  const response = await $.ajax({
-    url: '/tweets/',
-    type: 'GET',
-  });
-  renderTweets(response);
-};
 
 $(".arrows").click(() => {
   $(".new-tweet").slideToggle();
 });
+
 $(window).scroll(function () {
   if ($(this).scrollTop() > 300) { // 300px from top
     $('.fa-arrow-circle-up').fadeIn();
@@ -107,10 +107,14 @@ $(window).scroll(function () {
     $('.fa-arrow-circle-up').fadeOut();
   }
 });
+
 $(".fa-arrow-circle-up").click(() => {
   $('.new-tweet').slideDown();
-  $("html, body").animate({ scrollTop: 0 }, 200);
-});
 
+
+  $("html, body").animate({ scrollTop: 0 }, 200);
+  $('.tweet-text-area').focus();
+
+});
 
 loadTweets();

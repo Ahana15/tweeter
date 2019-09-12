368805833b71eda46c7d908d88134232ebabd487
Ahana Ghosh <ahanaghosh94@gmail.com>
2019-09-12 17:43:20 +0000

    moved scroll to top button into new js file

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -98,19 +98,5 @@ $(".arrows").click(() => {
   $('.tweet-text-area').focus();
 });
 
-$(document).scroll(function () {
-  if ($(this).scrollTop() > 500) { // 300px from top
-    $('.fa-arrow-circle-up').fadeIn();
-  } else {
-    $('.fa-arrow-circle-up').fadeOut();
-  }
-});
-
-$(".fa-arrow-circle-up").click(() => {
-  $('.new-tweet').slideDown();
-  $("html, body").animate({ scrollTop: 0 }, 200);
-  $('.tweet-text-area').focus();
-
-});
 
 loadTweets();
---
+++ public/scripts/composer.js
@@ -0,0 +1,15 @@
+
+$(document).scroll(function () {
+  if ($(this).scrollTop() > 500) { // 300px from top
+    $('.fa-arrow-circle-up').fadeIn();
+  } else {
+    $('.fa-arrow-circle-up').fadeOut();
+  }
+});
+
+$(".fa-arrow-circle-up").click(() => {
+  $('.new-tweet').slideDown();
+  $("html, body").animate({ scrollTop: 0 }, 200);
+  $('.tweet-text-area').focus();
+
+});
